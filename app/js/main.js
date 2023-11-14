cd0cdcf8acc10c8bc286210e63d4a77ca41fd524
Lapki Web <Lapki.web@gmail.com>
2023-11-14 16:58:45 +0800

    add header + section onescreen + adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,13 @@
 // WOW JS Конфликтует с оверфлоу хайден на бади или секций
+
+$(function () {
+  $('.menu__btn').on('click', (function () {
+    if (!$('.menu').hasClass('active')) {
+      $('.menu').addClass('active');
+      $('.menu__btn').addClass('active');
+    } else {
+      $('.menu').removeClass('active');
+      $('.menu__btn').removeClass('active');
+    }
+  }));
+})
